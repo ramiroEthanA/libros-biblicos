5dc0c6e569942795e6976024bafc5a191afb4a3d
RamiroEthan <ramiro.e.aparicio@gmail.com>
2024-05-04 20:38:10 -0400

    Practica 2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = 3000; // puede cambiar
 
 //array 
 let librosBiblicos = [
-    {id: 1 , nombre: 'Genesis', autor: 'Moises'},
-    {id: 2 , nombre: 'Exodo', autor: 'Moises'},
-    {id: 3 , nombre: 'Levitico', autor: 'Moises'},
+    {id: 1 , nombre: 'Genesis', autor: 'Moises', anioPublicacion: 2020},
+    {id: 2 , nombre: 'Exodo', autor: 'Juan', anioPublicacion: 2024},
+    {id: 3 , nombre: 'Levitico', autor: 'Moises', anioPublicacion: 1990},
 ];
 //manejo de JSON
 app.use(express.json());
@@ -44,6 +44,81 @@ app.put('/actualizar-libro/:id', (req, res) => {
         res.status(404).json({mensaje : 'Libro no encontrado'});
     }
 });
+// endpoint 5 Eliminar Libro
+app.delete('/eliminar-libro/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    lBiblico = librosBiblicos.filter( libro => libro.id !== id);
+    res.status(201).json({mensaje : 'se ha eliminado el libro'});
+    console.log(lBiblico);
+});
+//endpoint 6 
+app.get('/libros/publicacion/:anio', (req, res) => {
+    const year =  parseInt(req.params.anio);
+    const librosPublicados = librosBiblicos.filter( x => x.anioPublicacion === year);
+    if (librosPublicados.length > 0) {
+        res.json(librosPublicados);
+    } else {
+        res.status(404).json({mensaje : 'no se han encontrado libros publicados en ese anio'});
+    }
+});
+
+//endpoint 7
+app.get('/datos/', (req, res) => {
+    const nombre = 'Ramiro'; // nombre
+    const profesion = 'Analista de Sistemas'; // profesión
+    res.send(`¡Bienvenido/a, ${nombre}! Tu profesión actual es ${profesion}.`);
+});
+
+
+//endpoint 8
+app.get('/libros/autor/:autor', (req, res) => {
+    const autor = req.params.autor;
+    const librosPorAutor = librosBiblicos.filter(libro => libro.autor.toLowerCase() === autor.toLowerCase());
+    if (librosPorAutor.length > 0) {
+        res.json(librosPorAutor);
+    } else {
+        res.status(404).json({mensaje: 'No se encontraron libros para ese autor.'});
+    }
+});
+
+
+});
+// enpoint 9 otro
+app.get('/libro/cantidad', (req, res) => {
+    const cantidadLibro = librosBiblicos.reduce((total, libro) => total + 1, 0);
+    res.json({ cantidad: cantidadLibro });
+});
+
+
+// Endpoint 10
+app.get('/libros/nombre/:texto', (req, res) => {
+    const texto = req.params.texto.toLowerCase();
+    const librosPorNombre = librosBiblicos.filter(libro => libro.nombre.toLowerCase().includes(texto));
+    if (librosPorNombre.length > 0) {
+        res.json(librosPorNombre);
+    } else {
+        res.status(404).json({mensaje: 'No se encontraron libros con ese nombre.'});
+    }
+});
+
+
+
+// endpoint 11 otro 
+app.get('/libro/orden', (req, res) => {
+    const librosOrdenados = librosBiblicos.slice().sort((a, b) => {
+        const nombreA = a.nombre.toLowerCase();
+        const nombreB = b.nombre.toLowerCase();
+        if (nombreA < nombreB) {
+            return -1;
+        }
+        if (nombreA > nombreB) {
+            return 1;
+        }
+        return 0;
+    });
+    res.json(librosOrdenados);
+});
+
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto http://localhost:" + PORT);
